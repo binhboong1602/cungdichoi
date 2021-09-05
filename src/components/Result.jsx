cfc27e454b8a47d034f1f980852d455f219b8c3c
binhboong <babinh.1602@gmail.com>
2021-09-05 20:22:09 +0700

    update_link

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 export default function Result() {
-    const history = useHistory();
-    const [link, setLink] = useState('');
-    function handleClick(e) {
-        e.preventDefault();
-        history.push('/');
-    }
+  const history = useHistory();
+  const [link, setLink] = useState("");
+  function handleClick(e) {
+    e.preventDefault();
+    history.push("/");
+  }
 
-    useEffect(async () => {
-        const result = await axios.get('https://613437ae7859e700176a3813.mockapi.io/go');
-        const data = result.data;
-        const sessionId = data[data.length - 1].sessionID;
-        setLink(sessionId);
-    }, [])
+  useEffect(async () => {
+    const result = await axios.get(
+      "https://613437ae7859e700176a3813.mockapi.io/go"
+    );
+    const data = result.data;
+    const sessionId = data[data.length - 1].sessionID;
+    setLink(sessionId);
+  }, []);
 
-
-
-    return (
-        <div>
-            <h1>Share this link to invite your friends</h1>
-            <h2>http://localhost:3000/sessions/detail/{link}</h2>
-            <Button onClick={handleClick}>Create new session</Button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Share this link to invite your friends</h1>
+      <h2>
+        https://cungdichoi-git-main-cungdichoiv3.vercel.app/sessions/detail/
+        {link}
+      </h2>
+      <Button onClick={handleClick}>Create new session</Button>
+    </div>
+  );
 }
